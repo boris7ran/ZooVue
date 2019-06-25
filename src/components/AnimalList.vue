56090339e5569223e24f9eb069328f0b2a95f192
boris7ran <boris.ran7@gmail.com>
2019-06-25 18:55:01 +0200

    Zadatak 1

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(animal, index) in listOfAnimals" :key=index>
+                Species: {{ animal.species }}   Name: {{ animal.name }}   Date of Birth: {{ animal.birthDate }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            listOfAnimals: [
+                {species: 'mammal', name: 'horse', birthDate: '11.11.2011.'},
+                {species: 'insect', name: 'bettle', birthDate: '09.12.2004.'},
+                {species: 'fish', name: 'salmon', birthDate: '04.01.2001.'},
+                {species: 'mammal', name: 'monkey', birthDate: '10.03.2013.'},
+                {species: 'rodent', name: 'mouse', birthDate: '01.01.2010.'}
+            ]
+        }
+    }
+}
+</script>

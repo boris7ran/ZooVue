8ca10a9c9de030f1c1ee1788dfb193cb8d4e6080
boris7ran <boris.ran7@gmail.com>
2019-06-25 19:00:37 +0200

    Zadatak 2

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -2,7 +2,7 @@
     <div>
         <ul>
             <li v-for="(animal, index) in listOfAnimals" :key=index>
-                Species: {{ animal.species }}   Name: {{ animal.name }}   Date of Birth: {{ animal.birthDate }}
+                Species: {{ animal.species }}   Name: {{ animal.name }}   Date of Birth: {{ checkIfEmpty(animal.birthDate) }}
             </li>
         </ul>
     </div>
@@ -13,12 +13,22 @@ export default {
     data () {
         return {
             listOfAnimals: [
-                {species: 'mammal', name: 'horse', birthDate: '11.11.2011.'},
+                {species: 'mammal', name: 'horse', birthDate: ''},
                 {species: 'insect', name: 'bettle', birthDate: '09.12.2004.'},
                 {species: 'fish', name: 'salmon', birthDate: '04.01.2001.'},
                 {species: 'mammal', name: 'monkey', birthDate: '10.03.2013.'},
                 {species: 'rodent', name: 'mouse', birthDate: '01.01.2010.'}
             ]
+        }
+    },
+
+    methods: {
+        checkIfEmpty (date) {
+            if (!date) {
+                return 'Nepoznat';
+            } else {
+                return date;
+            }
         }
     }
 }

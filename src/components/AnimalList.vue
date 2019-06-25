b85fbeaf9e91a2b499f7a65e97e167d92b5d0e74
boris7ran <boris.ran7@gmail.com>
2019-06-25 19:23:04 +0200

    Zadatak 5

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <form @submit.prevent="addAnimal">
+            <div>
+                <label for="species">Species</label>
+                <input type="text" id="species" v-model="newAnimal.species" required>
+            </div>
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" v-model="newAnimal.name" required>
+            </div>
+            <div>
+                <label for="birthDate">Date of Birth</label>
+                <input type="date" id="birthDate" v-model="newAnimal.birthDate">
+            </div>
+
+            <div>
+                <button type="submit">Add Animal</button>
+            </div>
+        </form>
+
         <ul>
             <li v-for="(animal, index) in listOfAnimals" :key=index>
                 Species: {{ animal.species }}   Name: {{ animal.name }}   Date of Birth: {{ checkIfEmpty(animal.birthDate) }}
@@ -14,12 +33,18 @@
 export default {
     data () {
         return {
+            newAnimal: {
+                species: '',
+                name: '',
+                birthDate: ''
+            },
+
             listOfAnimals: [
-                {species: 'mammal', name: 'horse', birthDate: ''},
-                {species: 'insect', name: 'bettle', birthDate: '09.12.2004.'},
-                {species: 'fish', name: 'salmon', birthDate: '04.01.2001.'},
-                {species: 'mammal', name: 'monkey', birthDate: '10.03.2013.'},
-                {species: 'rodent', name: 'mouse', birthDate: '01.01.2010.'}
+                {species: 'Horse', name: 'Konjo', birthDate: ''},
+                {species: 'Fox', name: 'Lijo', birthDate: '09.12.2004.'},
+                {species: 'Lynx', name: 'Riso', birthDate: '04.01.2001.'},
+                {species: 'Bear', name: 'Grizo', birthDate: '10.03.2013.'},
+                {species: 'Monkey', name: 'Majmo', birthDate: '01.01.2010.'}
             ]
         }
     },
@@ -39,8 +64,16 @@ export default {
 
         moveToTop (index) {
             let tempAnimal = this.listOfAnimals.splice(index, 1);
-            console.log(tempAnimal);
             this.listOfAnimals = [...tempAnimal, ...this.listOfAnimals];
+        },
+
+        addAnimal () {
+            this.listOfAnimals.push({...this.newAnimal});
+            this.newAnimal = {
+                species: '',
+                name: '',
+                birthDate: ''
+            }
         }
     }
 }
